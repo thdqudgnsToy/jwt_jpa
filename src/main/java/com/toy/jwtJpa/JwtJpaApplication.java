fb40f0be09eaef4bbd84306e841828860e37f05d
HUN\Hun <thdqudgns9612@gmail.com>
2023-06-25 21:31:14 +0900

    project init

---
+++ src/main/java/com/toy/jwtJpa/JwtJpaApplication.java
@@ -0,0 +1,13 @@
+package com.toy.jwtJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtJpaApplication.class, args);
+	}
+
+}
